def411489cd1e04bef60078234c1004ff0eb235c
CAPreetham <88428335+CAPreetham@users.noreply.github.com>
2021-08-11 22:01:14 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,50 @@
+my_list = ['p', 'y', 't', 'h', 'o','n']
+
+# Output: p
+print(my_list[0])
+
+# Output: o
+print(my_list[2])
+
+# Output: e
+print(my_list[4])
+
+my2_list=['p', 'r', 'o', 'g', 'r','a','m']
+print(my_list+my2_list)
+
+print('we save python file with last filename.',my_list[:2])
+
+# Demonstration of list insert() method
+odd = [1, 9]
+odd.insert(1,3)
+
+print(odd)
+
+odd[2:2] = [5, 7]
+
+print(odd)
+
+my_list = ['p','r','o','b','l','e','m']
+my_list.remove('p')
+
+# Output: ['r', 'o', 'b', 'l', 'e', 'm']
+print(my_list)
+
+# Output: 'o'
+print(my_list.pop(1))
+
+# Output: ['r', 'b', 'l', 'e', 'm']
+print(my_list)
+
+# Output: 'm'
+print(my_list.pop())
+
+# Output: ['r', 'b', 'l', 'e']
+print(my_list)
+
+my_list.clear()
+
+# Output: []
+print(my_list)
+
 
